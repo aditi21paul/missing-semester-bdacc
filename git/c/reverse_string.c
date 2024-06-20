a75dc11351534d687fa9c7e20650ba561c0ad2c4
Aditi Paul <63842846+aditi21paul@users.noreply.github.com>
2024-06-20 15:10:40 +0530

    Updated code to reverse string
    The initial code were having a two major issues, that is in one line a semicolon was missing and the other was with the for loop in which the string was actually being reversed. I tried to correct the loop conditions by updating it so that even the odd length strings have no issues in reversal. Since the time complexity is of order of O(n) I didn't think it required optimization.
    
    Note: I have registered with a different email id in the form i.e. , ap23csb0f47@student.nitw.ac.in and am using this email id for Git Hub.

--- git/c/reverse_string.c
+++ git/c/reverse_string.c
@@ -1,6 +1,5 @@
 // Script: reverse_string.c
 // Reverses a given string
-// Hint: There are multiple issues with this implementation
 
 #include <stdio.h>
 #include <string.h>
@@ -8,10 +7,10 @@
 void reverse_string(char str[]) {
     int i, j;
     char temp;
-    for (i = 0, j = strlen(str) - 1; i < j; i++, j--) {
+    for (i = 0, j = strlen(str) - 1; i <= j; i++, j--) {
         temp = str[i];
-        str[i] = str[j]
-        str[j] = temp;  // Hint: A semicolon is missing somewhere.
+        str[i] = str[j];
+        str[j] = temp;  
     }
 }
 
